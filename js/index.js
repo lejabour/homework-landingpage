e3f4d45cfa5193b5a7bb74ea1928be84bd6d1756
Jason Bourguignon <jasonbourguignon@gmail.com>
2016-03-20 20:56:09 -0700

    latest

---
+++ js/index.js
@@ -0,0 +1,30 @@
+//Implement the toggle for the articles. Don't forget to add the script tags.
+
+
+$(document).ready(function(){
+ 
+$(".more1").hide();
+$( "h5.read1" ).click(function() {
+  $( ".more1" ).toggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+
+
+$(".more2").hide();
+$( "h5.read2" ).click(function() {
+  $( ".more2" ).toggle( "slow", function() {
+    // Animation complete.
+  });
+});
+
+
+
+
+
+
+console.log('hello');
+
+
+});
