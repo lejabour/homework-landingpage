5845f8ea0c765c5d76837554b78aff1c13a71432
Jason Bourguignon <jasonbourguignon@gmail.com>
2016-04-10 19:52:48 -0700

    updated homework

--- js/index.js
+++ js/index.js
@@ -3,23 +3,51 @@
 
 $(document).ready(function(){
  
+$(".readLess1").hide();
 $(".more1").hide();
-$( "h5.read1" ).click(function() {
-  $( ".more1" ).toggle( "slow", function() {
+$( "h5.read1" ).click(function(event) {
+  event.preventDefault();
+  $( ".more1" ).toggle();
+  $( "h5.read1" ).toggle();
+    $( ".readLess1" ).toggle();
     // Animation complete.
   });
-});
 
+$( ".readLess1" ).click(function(event) {
+  event.preventDefault();
+  $( ".more1" ).toggle();
+  $( ".readLess1" ).toggle();
+  $( "h5.read1" ).toggle();
+});
 
 
 $(".more2").hide();
+$(".readLess2").hide();
 $( "h5.read2" ).click(function() {
-  $( ".more2" ).toggle( "slow", function() {
+ event.preventDefault();
+  $( ".more2" ).toggle();
+  $( "h5.read2" ).toggle();
+    $( ".readLess2" ).toggle();
     // Animation complete.
   });
+
+
+$( ".readLess2" ).click(function(event) {
+  event.preventDefault();
+  $( ".more2" ).toggle();
+  $( ".readLess2" ).toggle();
+  $( "h5.read2" ).toggle();
 });
 
+ 
 
+$(".more3").hide();
+$( "h5.read3" ).click(function(event) {
+  event.preventDefault();
+  $( ".more3" ).toggle();
+  $( "h5.read3" ).toggle();
+    //animation complete
+});
 
 console.log('hello');
 
